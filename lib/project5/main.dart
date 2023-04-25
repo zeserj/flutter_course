bd758e6e091fe8dbeff06ca8bc8f66da23c97e67
Sergiu Popa <sj_popa@yahoo.com>
2023-04-25 18:57:13 +0300

    added ability to select image count and a query for the type of images

--- lib/project5/main.dart
+++ lib/project5/main.dart
@@ -7,12 +7,16 @@ import 'package:http/http.dart' as http;
 class UnsplashApi {
   static final String? _accessKey = dotenv.env['UNSPLASH_ACCESS_KEY'];
 
-  static Future<List<String>> getRandomImageUrls() async {
+  static Future<List<String>> getRandomImageUrls(int count, String query) async {
     final Uri url = Uri(
       scheme: 'https',
       host: 'api.unsplash.com',
       path: '/photos/random',
-      queryParameters: <String, String?>{'client_id': '$_accessKey', 'count': '20'},
+      queryParameters: <String, String?>{
+        'client_id': '$_accessKey',
+        'count': count.toString(),
+        'query': query,
+      },
     );
 
     final http.Response response = await http.get(url);
@@ -46,6 +50,11 @@ class _MyAppState extends State<MyApp> {
   late List<String> _imageUrls;
   late bool _isLoading;
 
+  final List<int> _countOptions = <int>[4, 10, 20, 30];
+  final List<String> _queryOptions = <String>['images', 'cat images', 'dog images'];
+  int _count = 4;
+  String _query = 'images';
+
   @override
   void initState() {
     super.initState();
@@ -57,7 +66,7 @@ class _MyAppState extends State<MyApp> {
       setState(() {
         _isLoading = true;
       });
-      final List<String> imageUrls = await UnsplashApi.getRandomImageUrls();
+      final List<String> imageUrls = await UnsplashApi.getRandomImageUrls(_count, _query);
       setState(() {
         _imageUrls = imageUrls;
         _isLoading = false;
@@ -70,24 +79,78 @@ class _MyAppState extends State<MyApp> {
   }
 
   Widget _buildImageGrid() {
-    return GridView.count(
-      padding: const EdgeInsets.all(10),
-      crossAxisSpacing: 4,
-      mainAxisSpacing: 4,
-      crossAxisCount: 2,
-      children: _imageUrls
-          .map<Widget>((String imageUrl) => Image.network(
-                imageUrl,
-                fit: BoxFit.cover,
-                loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return const CircularProgressIndicator();
-                },
-                errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) => const Icon(Icons.error),
-              ))
-          .toList(),
+    return Expanded(
+        flex: 4,
+        child: GridView.count(
+          padding: const EdgeInsets.all(10),
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+          crossAxisCount: 2,
+          children: _imageUrls
+              .map<Widget>((String imageUrl) => Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+            loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return const CircularProgressIndicator();
+            },
+            errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) => const Icon(Icons.error),
+          )).toList(),
+        )
+    );
+  }
+
+  Widget _buildDropdowns() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        const Text('Generate '),
+        DropdownButton<int>(
+          alignment: AlignmentDirectional.center,
+          value: _count,
+          icon: const Icon(Icons.arrow_downward),
+          style: const TextStyle(color: Colors.blue),
+          underline: Container(
+            height: 2,
+            color: Colors.blueAccent,
+          ),
+          onChanged: (int? value) {
+            setState(() {
+              _count = value!;
+            });
+            },
+          items: _countOptions.map<DropdownMenuItem<int>>((int value) {
+            return DropdownMenuItem<int>(
+              value: value,
+              child: Text(value.toString()),
+            );
+          }).toList(),
+        ),
+        const Text(' random '),
+        DropdownButton<String>(
+          alignment: AlignmentDirectional.center,
+          value: _query,
+          icon: const Icon(Icons.arrow_downward),
+          style: const TextStyle(color: Colors.blue),
+          underline: Container(
+            height: 2,
+            color: Colors.blueAccent,
+          ),
+          onChanged: (String? value) {
+            setState(() {
+              _query = value!;
+            });
+          },
+          items: _queryOptions.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ],
     );
   }
 
@@ -97,7 +160,12 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(title: const Text('Unsplash App'), centerTitle: true),
         body: Center(
-          child: _isLoading ? const CircularProgressIndicator() : _buildImageGrid(),
+          child: Column(
+            children: <Widget>[
+              _buildDropdowns(),
+              if (_isLoading) const CircularProgressIndicator() else _buildImageGrid(),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _loadImage,
